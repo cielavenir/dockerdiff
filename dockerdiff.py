fe023b86a5406468c2563223c22e706a7ddadcf6
cielavenir <cielartisan@gmail.com>
2021-02-19 15:20:11 +0900

    initial

---
+++ dockerdiff.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import sys
+import os
+import stat
+import json
+import subprocess
+
+def getDiff(fout, container, filename, *args):
+    dockerDir = subprocess.check_output(['docker','info','--format','{{.DockerRootDir}}'],shell=False).decode('utf-8').strip()
+    if os.getuid() != os.stat(dockerDir).st_uid:
+        raise Exception('dockerdiff user (%d) is different from DockerRootDir (%s) user (%d).\nNote: dockerdiff to usual (non-rootless) dockerd requires sudo.'%(os.getuid(),dockerDir,os.stat(dockerDir).st_uid))
+
+    # need to run user which can access docker volume
+    if filename[0]!='/':
+        raise Exception('filename needs to start with "/"')
+
+    inspe = json.loads(subprocess.check_output(['docker','inspect',container]).decode('utf-8'))
+    graphDriver = inspe[0]['GraphDriver']
+    if 'Name' in graphDriver and not graphDriver['Name'].startswith('overlay'):
+        raise Exception('graphDriver not overlay')
+    data = graphDriver.get('Data',{})
+    if 'UpperDir' not in data or 'LowerDir' not in data:
+        raise Exception('UpperDir or LowerDir is not published')
+
+    upper = data['UpperDir']+filename
+    # deduce lower
+    print data['LowerDir']
+    for lowerDir in data['LowerDir'].split(':'): # first is outer image, maybe
+        lower = lowerDir+filename
+        if os.path.exists(lower):
+            break
+    else:
+        lower = None
+
+    if not os.path.exists(upper):
+        if lower is None:
+            # file does not exist
+            raise Exception('%s does not exist in container (%s)'%(filename,container))
+        else:
+            # file is not modified, no need to run diff
+            return
+    else:
+        if lower is None:
+            # file is added
+            lower = '/dev/null'
+        else:
+            # file is modified (or deleted)
+            pass
+    if stat.S_ISCHR(os.stat(upper).st_mode):
+        # file is deleted
+        # todo better logic?
+        upper = '/dev/null'
+    
+    subprocess.call(['diff',lower,upper]+list(args),stdout=fout)
+
+if __name__=='__main__':
+    if len(sys.argv)<3:
+        sys.stderr.write('Usage: [sudo] dockerdiff container filename [diff-args...]\n')
+        exit(1)
+
+    prog, container, filename = sys.argv[:3]
+    args = sys.argv[3:]
+    getDiff(sys.stdout, container, filename, *args)
